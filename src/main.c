3fe16f411879796b0985e9c1a080deb652e35ab2
ib <ib.salat272@gmail.com>
2026-01-06 18:02:44 -0500

    setup handle command

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 #define BUFFER_SIZE 1024
 
+int handle_echo(char *command);
 
 
 int main(int argc, char *argv[]) {
@@ -26,7 +27,34 @@ int main(int argc, char *argv[]) {
         if (strcmp(command, "exit") == 0){
         return 0;
         }
+
+        //handle echo command
+        if (handle_echo(command)){
+        continue;
+        }
     }
 
+    return 0;
+}
 
+
+
+int handle_echo(char *command){
+  /*
+  (char *) -> int
+  Function checks whether the given command is an echo command. If so, prints the text following echo to standard output and returns 1. 
+  If the command is not an echo command, returns 0.
+  */
+
+  if (strncmp(command, "echo", 4) != 0 || (command[4] != ' ' && command[4] != '\0')){
+    return 0;
+  }
+
+  if (command[4] == ' '){
+    printf("%s\n", command + 5);
+  } else{
+    printf("\n");
+  }
+
+  return 1;
 }
